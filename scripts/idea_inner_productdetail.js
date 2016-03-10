1eeb2940c8a9ef647d66903be0454852f9b91fdf
h5m1007 <355795973@qq.com>
2016-03-10 10:49:55 +0800

    2016/03/10 10:49

--- scripts/idea_inner_productdetail.js
+++ scripts/idea_inner_productdetail.js
@@ -1,44 +1,44 @@
-$(function(){
-	$('#loan_amount').ionRangeSlider({
-  		min: 5000,
-  		max: 100000,
-  		step: 1000,
-  		prefix: '$',
-  		from: 20000,
-  		prettify_separator: ',',
-	});
-	$('#mobile_loan_amount').ionRangeSlider({
-  		min: 5000,
-  		max: 100000,
-  		step: 1000,
-  		prefix: '$',
-  		from: 20000,
-  		prettify_separator: ',',
-	});
-	var ddData_loan =[
-          		{ text: '1 year', value: 1,},
-          		{ text: '2 years', value: 2,},
-          		{ text: '3 years', value: 3,},
-          		{ text: '4 years', value: 4,},
-          		{ text: '5 years', value: 5,},
-          		{ text: '6 years', value: 6,},
-	];
-	$('#tenor').ddslick({
-		data: ddData_loan,
-		width: '120px',
-		color: '#3c4248',
-		background: 'white',
-		selectText: '3 years',
-	});
-	$('#mobile_tenor').ddslick({
-		data: ddData_loan,
-		width: '120px',
-		color: '#3c4248',
-		background: 'white',
-		selectText: '3 years',
-	});
+$(function() {
+    $('#loan_amount').ionRangeSlider({
+        min: 5000,
+        max: 100000,
+        step: 1000,
+        prefix: '$',
+        from: 20000,
+        prettify_separator: ',',
+    });
+    $('#mobile_loan_amount').ionRangeSlider({
+        min: 5000,
+        max: 100000,
+        step: 1000,
+        prefix: '$',
+        from: 20000,
+        prettify_separator: ',',
+    });
+    var ddData_loan = [
+        { text: '1 year', value: 1, },
+        { text: '2 years', value: 2, },
+        { text: '3 years', value: 3, },
+        { text: '4 years', value: 4, },
+        { text: '5 years', value: 5, },
+        { text: '6 years', value: 6, },
+    ];
+    $('#tenor').ddslick({
+        data: ddData_loan,
+        width: '120px',
+        color: '#3c4248',
+        background: 'white',
+        selectText: '3 years',
+    });
+    $('#mobile_tenor').ddslick({
+        data: ddData_loan,
+        width: '120px',
+        color: '#3c4248',
+        background: 'white',
+        selectText: '3 years',
+    });
 
-	$(".contentContainer").scroll(function() {
+    $(".contentContainer").scroll(function() {
         var viewH = $(this).height(), //可见高度  
             contentH = $(this).get(0).scrollHeight, //内容高度  
             scrollTop = $(this).scrollTop(); //滚动高度  
@@ -48,5 +48,5 @@ $(function(){
             $('.scroll').show();
         }
     });
-	
+
 })
